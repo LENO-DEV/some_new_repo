80d0b9b695076a7b1cc45842384c86ddbc7fbbec
Mondal <debopiku1122@gmail.com>
2021-01-25 11:51:55 +0530

    lamnda_func updated

--- lambda_fun.js
+++ lambda_fun.js
@@ -1,11 +1,20 @@
-module.exports.functionOne = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-    }),
-  };
+const AWS = require('aws-sdk');
+AWS.config.region = 'ap-south-1';
+const DynamoDB = new AWS.DynamoDB.DocumentClient();
 
-  callback(null, response);
-
+module.exports.functionOne = async (event) => {
+  return new Promise((res, rej) => {
+    var Params = {
+      TableName: 'Project_Data'
+    }
+    DynamoDB.scan(Params, (err, data) => {
+      if (err) rej(err);
+      else {
+        const response = {
+          body:   JSON.stringify(data,null)
+        }
+        res(response);
+      }
+    });
+  });
 };
