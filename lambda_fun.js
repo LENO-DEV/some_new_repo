9fc5aae5da0525348fe4de1ab012e829ba1ab5d4
Mondal <debopiku1122@gmail.com>
2021-01-25 10:52:18 +0530

    lamnda_func updated

--- lambda_fun.js
+++ lambda_fun.js
@@ -1,5 +1,11 @@
 module.exports.functionOne = async (event) => {
-  return {
-    message: 'Hello World 333!!!'
-  };
+  return new Promise((res, rej) => {
+    var a = 5, b = 6;
+    var sum = a + b;
+    if (sum) {
+      res("wow " + sum);
+    }
+    else rej('Ooops! something went wrong!!!');
+  });
 }
+// fun().then((el) => console.log(el));
